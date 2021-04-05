79517ec0a574201dd1cc99a6f678c99f3fd60632
k-yomo <kanji.yy@gmail.com>
2021-04-06 00:46:19 +0900

    Implement a publish intereptor to set unified attributes

---
+++ middleware/pm_attributes/pm_attributes.go
@@ -0,0 +1,22 @@
+package pm_attributes
+
+import (
+	"cloud.google.com/go/pubsub"
+	"context"
+	"github.com/k-yomo/pm"
+)
+
+// PublishInterceptor set given attributes to the all publishing messages.
+// This interceptor doesn't overwrite if already the same key's attribute is set.
+func PublishInterceptor(attrs map[string]string) pm.PublishInterceptor {
+	return func(next pm.MessagePublisher) pm.MessagePublisher {
+		return func(ctx context.Context, topic *pubsub.Topic, m *pubsub.Message) *pubsub.PublishResult {
+			for k, v := range attrs {
+				if _, ok := m.Attributes[k]; !ok {
+					m.Attributes[k] = v
+				}
+			}
+			return next(ctx, topic, m)
+		}
+	}
+}
